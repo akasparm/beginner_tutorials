11cc393bef6aaa0bdae7fed67177f9b30cae4286
Akashkumar Parmar <112916785+akasparm@users.noreply.github.com>
2023-11-06 22:10:02 -0500

    Subscriber

--- src/subscriber_member_function.cpp
+++ src/subscriber_member_function.cpp
@@ -14,27 +14,46 @@
 
 #include <functional>
 #include <memory>
-
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
 #include "std_msgs/msg/string.hpp"
 
 using std::placeholders::_1;
 
+/**
+ * @brief Subscriber class
+ *
+ */
 class MinimalSubscriber : public rclcpp::Node {
  public:
+  /**
+   * @brief Construct a new Minimal Subscriber object
+   *
+   */
   MinimalSubscriber() : Node("minimal_subscriber") {
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
   }
 
  private:
+  /**
+   * @brief topic_callback function, calls the topic and fetches the message
+   * from it
+   *
+   * @param msg
+   */
   void topic_callback(const std_msgs::msg::String& msg) const {
-    RCLCPP_INFO(this->get_logger(), "I heard the modification: '%s'",
-                msg.data.c_str());
+    RCLCPP_INFO(this->get_logger(), "I heard the modification: '%s'", msg.data.c_str());
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
 
+/**
+ * @brief main function
+ *
+ * @param argc
+ * @param argv
+ * @return int
+ */
 int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalSubscriber>());

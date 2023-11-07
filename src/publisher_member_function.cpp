cac774ed3c2602adc70ed055303e4d4f91af4924
Akashkumar Parmar <112916785+akasparm@users.noreply.github.com>
2023-11-06 22:04:34 -0500

    Publisher

--- src/publisher_member_function.cpp
+++ src/publisher_member_function.cpp
@@ -19,24 +19,55 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "cpp_pubsub/srv/StringMod.hpp"
 
+using std::placeholders::_1;
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
- * member function as a callback from the timer. */
+using sharedFuture = rclcpp::Client<cpp_pubsub::srv::ModifyMsg>::SharedFuture;
+
 
 class MinimalPublisher : public rclcpp::Node {
  public:
   MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+    // Parameter Declaration
+    auto param_desc = rcl_interfaces::msg::ParameterDescriptor();
+    param_desc.description = "Set callback frequency.";
+    this->declare_parameter("freq", 2.0, param_desc);
+    auto param = this->get_parameter("freq");
+    auto freq = param.get_parameter_value().get<std::float_t>();
+    RCLCPP_DEBUG(this->get_logger(),
+                 "Parameter frequency is set to 2.0 hz");
+
+    // creating a subscriber for tthe parameter
+    parameter_subscriber_ =
+        std::make_shared<rclcpp::ParameterEventHandler>(this);
+    auto parameterCallbackPtr =
+        std::bind(&MinimalPublisher::parameter_callback, this, _1);
+    parameterHandle_ = parameter_subscriber_->add_parameter_callback(
+        "freq", parameterCallbackPtr);
+
     publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    RCLCPP_DEBUG(this->get_logger(), "Publisher is Created");
+    auto delta = std::chrono::milliseconds(static_cast<int>((1000 / freq)));
     timer_ = this->create_wall_timer(
-        500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        delta, std::bind(&MinimalPublisher::timer_callback, this));
+
+    client = this->create_client<cpp_pubsub::srv::ModifyMsg>("modify_msg");
+    RCLCPP_DEBUG(this->get_logger(), "Client created");
+    while (!client->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_FATAL(rclcpp::get_logger("rclcpp"), "Interrupted");
+        exit(EXIT_FAILURE);
+      }
+      RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "No service Available yet..!");
+    }
   }
 
  private:
   void timer_callback() {
     auto message = std_msgs::msg::String();
-    message.data = "I am modified string " + std::to_string(count_++);
+    message.data = "Publishing modified string: " + std::to_string(count_++);
     RCLCPP_INFO(this->get_logger(), "Publishing the modification: '%s'",
                 message.data.c_str());
     publisher_->publish(message);

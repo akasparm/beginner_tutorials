f35b6917bbbdee27791bae169750586cf40f5dc4
Akashkumar Parmar <112916785+akasparm@users.noreply.github.com>
2023-11-20 18:39:42 -0500

    indentation update

--- test/main.cpp
+++ test/main.cpp
@@ -17,15 +17,14 @@
 
 #include <rclcpp/rclcpp.hpp>
 
-    /**
-     * @brief Main funtion to run all the tests
-     *
-     * @param argc
-     * @param argv
-     * @return int
-     */
-    int
-    main(int argc, char** argv) {
+  /**
+    * @brief Main funtion to run all the tests
+    *
+    * @param argc
+    * @param argv
+    * @return int
+    */
+int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
   ::testing::InitGoogleTest(&argc, argv);
   int result = RUN_ALL_TESTS();
